8920e52b51314805062d0aac9a2c31cd82856454
Eliseo Garcia <zeog14@gmail.com>
2019-10-14 14:03:44 -0500

    Sketched a new class, Student, with rating

---
+++ Student.java
@@ -0,0 +1,15 @@
+
+public class Student implements Comparable<Students>{
+	
+	private String className;
+	private Integer rating;
+
+	@Override
+	public int compareTo(Students o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+
+}
