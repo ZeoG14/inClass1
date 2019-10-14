63ee0b6f4eb7db702185779e798e24ca4647741b
Eliseo Garcia <zeog14@gmail.com>
2019-10-14 13:47:47 -0500

    Added Person.java and created PersonTest.java

---
+++ Person.java
@@ -0,0 +1,33 @@
+
+public class Person extends NameHolder 
+{
+	private String lastName;
+	
+	
+	public Person() throws NameException
+	{
+		String temp1 = "Bo";
+		String temp2 = "Smith";
+		
+		lastName = temp1;
+		super.setFirstName(temp2);
+		
+	}
+	
+	public Person(String firstName, String lastName) throws NameException
+	{
+		this.lastName = lastName; 
+		
+		super.setFirstName(lastName);
+		
+	}
+	
+	@Override
+	public String describeSelf() {
+		String description = "First Name: " + super.getFirstName() + "Last Name: " + lastName;
+		return description;
+	}
+	
+	
+
+}
